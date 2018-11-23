ec9489766445ecf857db3d4e538646e117d611c3
lbs1163 <lbs1163@postech.ac.kr>
2018-11-24 03:06:42 +0900

    Modify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,21 @@
+var path = require("path");
+var webpack = require("webpack");
+var BundleTracker = require("webpack-bundle-tracker");
+
 module.exports = {
+  context: __dirname,
+
+  entry: './react/index.js',
+
+  output: {
+    path: path.resolve('./snippets/static/snippets/js/bundle'),
+    filename: "[name]-[hash].js",
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+
   module: {
     rules: [
       {
