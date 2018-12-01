dd09d901a07869708aa1a2df6f552104a87431b5
lbs1163 <lbs1163@postech.ac.kr>
2018-12-01 12:25:25 +0900

    Change url of APIs

--- snippets/urls.py
+++ snippets/urls.py
@@ -11,6 +11,6 @@ router.register(r'users', views.UserViewSet)
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url('api/', include(router.urls)),
     url('tictactoe/', views.tictactoe),
 ]
