51560abf218a1de474cf546205984761a19fdce1
lbs1163 <lbs1163@postech.ac.kr>
2018-12-01 12:50:57 +0900

    Make views

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework import generics, permissions, renderers, viewsets
 from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
@@ -41,3 +42,30 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
 def tictactoe(request):
     return render(request, 'snippets/tictactoe.html', {})
+
+def user_list(request):
+    return render(request, 'snippets/base.html', {'div_id': 'user_list'})
+
+def user_detail(request, pk=None):
+    if not User.objects.filter(pk=pk).exists():
+        raise Http404()
+
+    return render(request, 'snippets/base.html', {'div_id': 'user_detail'})
+
+def snippet_list(request):
+    return render(request, 'snippets/base.html', {'div_id': 'snippet_list'})
+
+def snippet_detail(request, pk=None):
+    if not User.objects.filter(pk=pk).exists():
+        raise Http404()
+
+    return render(request, 'snippets/base.html', {'div_id': 'snippet_detail'})
+
+def snippet_create(request):
+    return render(request, 'snippets/base.html', {'div_id': 'snippet_create'})
+
+def snippet_edit(request, pk=None):
+    if not User.objects.filter(pk=pk).exists():
+        raise Http404()
+
+    return render(request, 'snippets/base.html', {'div_id': 'snippet_edit'})
